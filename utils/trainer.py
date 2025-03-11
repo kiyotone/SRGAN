d9f3d4ae603fd11b68638eadae54b8a3fb417436
kiyotone <71599237+kiyotone@users.noreply.github.com>
2025-03-11 23:00:46 +0545

    main push

--- utils/trainer.py
+++ utils/trainer.py
@@ -13,18 +13,22 @@ class SRGANTrainer:
         self.device = device
         self.output_dir = f"out/{version}/models"
         os.makedirs(self.output_dir, exist_ok=True)
+        self.latest_epoch = 0
         
-        
-
+    
         # Initialize loss functions
         self.losses = SRGANLosses(device, vgg_loss_weight)
 
     def train(self, train_loader, num_epochs=10):
         gen_losses, disc_losses = [], []
+        
+        start_epoch = self.latest_epoch        
+        end_epoch = start_epoch + num_epochs
 
-        for epoch in range(num_epochs):
+        for epoch in range(start_epoch , end_epoch):
             gen_loss_total = 0.0
             disc_loss_total = 0.0
+            
 
             progress_bar = tqdm(train_loader, total=len(train_loader), desc=f"Epoch {epoch+1}")
 
@@ -80,3 +84,25 @@ class SRGANTrainer:
             gen_losses = [float(loss) for loss in lines[1:lines.index("\n")]]
             disc_losses = [float(loss) for loss in lines[lines.index("\n")+1:]]
         return gen_losses, disc_losses
+    
+
+    def load_model(self, model, path):
+        model.load_state_dict(torch.load(path))
+        return model
+    
+    def load_generator(self , epoch):
+        return self.load_model(self.generator, f"{self.output_dir}/generator_epoch_{epoch}.pth")
+    
+    def load_discriminator(self , epoch):
+        return self.load_model(self.discriminator, f"{self.output_dir}/discriminator_epoch_{epoch}.pth")
+    
+    def load_latest_models(self):
+        gen_epoch = max([int(file.split("_")[-1].split(".")[0]) for file in os.listdir(self.output_dir) if "generator" in file])
+        disc_epoch = max([int(file.split("_")[-1].split(".")[0]) for file in os.listdir(self.output_dir) if "discriminator" in file])
+        print(f"Loading generator model from epoch {gen_epoch}")
+        print(f"Loading discriminator model from epoch {disc_epoch}")
+        self.load_generator(gen_epoch)
+        self.load_discriminator(disc_epoch)
+        self.latest_epoch = max(gen_epoch, disc_epoch)
+        return gen_epoch, disc_epoch
+        
